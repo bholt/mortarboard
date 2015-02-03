e3743c5af68f0a9fbf28603c587d604376bd65a4
Brandon Holt <bholt@cs.washington.edu>
2015-02-03 01:03:53 -0800

    mostly-working example of building pdf & html from Rmd

---
+++ Makefile
@@ -0,0 +1,41 @@
+
+PDFLATEX	?= pdflatex -halt-on-error -file-line-error
+BIBTEX		?= bibtex
+PANDOC    ?= pandoc --natbib -S --standalone
+
+ifneq ($(QUIET),)
+PDFLATEX	+= -interaction=batchmode
+ERRFILTER	:= > /dev/null || (egrep ':[[:digit:]]+:' *.log && false)
+BIBTEX		+= -terse
+else
+PDFLATEX	+= -interaction=nonstopmode
+ERRFILTER=
+endif
+
+PAPER += paper
+
+.PHONY: all
+	
+all: pdf web
+web: $(PAPER).html
+pdf: $(PAPER).pdf
+
+$(PAPER).md: $(PAPER).Rmd Makefile
+	Rscript --slave -e "library(knitr); source('common.R'); knit('"$<"')"
+
+$(PAPER).tex: $(PAPER).md template.tex Makefile
+	$(PANDOC) --template=template.tex -o $@ $<
+
+$(PAPER).html: $(PAPER).md template.html Makefile
+	# $(PANDOC) --template=template.tex -o $@ $<
+	# Rscript --slave -e "library(knitr); pandoc('"$<"')"
+	$(PANDOC) --template=template.html -o $@ $<
+
+%.pdf: %.tex
+	$(PDFLATEX) $^
+	$(BIBTEX) $(basename $^)
+	$(PDFLATEX) $^
+	$(PDFLATEX) $^
+
+clean:
+	rm -f $(PAPER).{tex,pdf,log,out,html,md,aux,bbl,blg}
---
+++ common.R
@@ -0,0 +1,61 @@
+suppressPackageStartupMessages(require(RMySQL))
+suppressPackageStartupMessages(require(sqldf))
+suppressPackageStartupMessages(require(ggplot2))
+suppressPackageStartupMessages(require(reshape))
+options(RMySQL.dbname="claret") # (rest comes from $HOME/.my.cnf)
+
+db <- function(query, factors=c(), numeric=c()) {
+  d <- sqldf(query)
+  d[factors] <- lapply(d[factors], factor)
+  d[numeric] <- lapply(d[numeric], as.numeric)
+  return(d)
+}
+
+num <- function(var) as.numeric(as.character(var))
+
+save <- function(g, name=FILE_BASE, file=sprintf("%s/%s.pdf",FILE_DIR,name), w=3.3, h=3.1) {
+  ggsave(plot=g, filename=file, width=w, height=h)
+  print(sprintf("saved: %s", file))
+}
+
+prettify <- function(str) gsub('_',' ',gsub('([a-z])([a-z]+)',"\\U\\1\\E\\2",str,perl=TRUE))
+
+regex_match <- function(reg,str) length(grep(reg,str)) > 0
+
+label_pretty <- function(variable, value) {
+  vname <- if (regex_match('variable|value',variable)) '' else sprintf('%s:', variable)
+  lapply(paste(vname, prettify(as.character(value))), paste, collapse="\n")
+}
+
+geom_mean <- function(geom) stat_summary(fun.y='mean', geom=geom, labeller=label_pretty)
+
+geom_meanbar <- function(labeller=label_pretty) {
+  return(list(
+    stat_summary(fun.y=mean, geom='bar', position='dodge'),
+    stat_summary(fun.data=mean_cl_normal, geom='errorbar', width=0.2, position='dodge')
+  ))
+}
+
+# The palette with grey:
+cbPalette <- c("#0072B2", "#E69F00", "#009E73", "#D55E00", "#CC79A7", "#56B4E9", "#F0E442", "#999999")
+
+theme_mine <- list(
+  # To use for fills, add
+  scale_fill_manual(values=cbPalette),
+  # To use for line and point colors, add
+  scale_colour_manual(values=cbPalette),
+  # basic black and white theme
+  theme(
+    panel.background = element_rect(fill="white"),
+    panel.border = element_rect(fill=NA, color="grey50"),
+    panel.grid.major = element_line(color="grey80", size=0.2),
+    panel.grid.minor = element_line(color="grey90", size=0.2),
+    strip.background = element_rect(fill="grey90", color="grey50"),
+    strip.background = element_rect(fill="grey80", color="grey50"),
+    axis.ticks = element_line(colour="black"),
+    panel.grid = element_line(colour="black"),
+    axis.text.y = element_text(colour="black"),
+    axis.text.x = element_text(colour="black"),
+    text = element_text(size=11, family="Helvetica")
+  )
+)
